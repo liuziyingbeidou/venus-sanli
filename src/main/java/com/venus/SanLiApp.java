fb93ff69dffecd8390b920296851a5deef7484ee
liuziying <liuziyingbeidou@163.com>
2017-03-16 17:27:54 +0800

    Initial commit

---
+++ src/main/java/com/venus/SanLiApp.java
@@ -0,0 +1,13 @@
+package com.venus;
+
+/**
+ * Hello world!
+ *
+ */
+public class SanLiApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
